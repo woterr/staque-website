e915c9f6188d09c0078775354e526349f416a1ac
woterr <shreyasv320@gmail.com>
2022-05-28 22:01:45 +0530

    wefjwef

--- index.js
+++ index.js
@@ -3,18 +3,11 @@ const app = express();
 
 const rootRoutes = require(`./routes/root-routes.js`);
 const authRoutes = require(`./routes/auth-routes.js`);
-const cookies = require("cookies");
-const bodyParser = require("body-parser");
-const methodOverride = require("method-override");
 const port = process.env.PORT || 3000;
 
 __dirname;	
 app.set("views", __dirname + "/views");
 app.set("view engine", "pug");
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
-app.use(cookies.express("a", "b", "c"));
 
 app.use(express.static(`${__dirname}/assets`));
 app.locals.basedir = `${__dirname}/assets`;
