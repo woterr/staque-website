2b366d333e04f530ac52a9dc83d4a2878f823b8c
woterr <shreyasv320@gmail.com>
2022-05-28 22:03:24 +0530

    wefjwef

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,10 +1,9 @@
 const express = require("express");
-require("dotenv").config();
 const router = express.Router();
 
 router.get("/invite", (req, res) =>
   res.redirect(
-    `https://discord.com/api/oauth2/authorize?client_id=${process.env.botid}&permissions=2416298064&scope=applications.commands%20bot`
+    `https://discord.com/api/oauth2/authorize?client_id=923208617920987136&permissions=2416298064&scope=applications.commands%20bot`
   )
 );
 
